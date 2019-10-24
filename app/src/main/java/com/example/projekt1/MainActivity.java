f0f5fd585c0f921564846af860cd8132a9dd9c7f
SaskaElod <saskaelod98@gmail.com>
2019-10-24 13:36:07 +0300

    asdasd

--- app/src/main/java/com/example/projekt1/MainActivity.java
+++ app/src/main/java/com/example/projekt1/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.projekt1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginbutton;
+    EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent intent=new Intent(this,VotingActivity.class);
+        loginbutton=findViewById(R.id.login_button);
+        username=findViewById(R.id.name_editText);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name;
+                name =username.getText().toString();
+                intent.putExtra("username",name);
+                Log.d("user",name);
+                startActivity(intent);
+            }
+        });
     }
 }
