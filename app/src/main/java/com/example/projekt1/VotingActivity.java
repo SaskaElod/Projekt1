92d40a73d6cfeac35af69d13d44c96105b4033f0
SaskaElod <saskaelod98@gmail.com>
2019-10-24 17:06:02 +0300

    asdasd

--- app/src/main/java/com/example/projekt1/VotingActivity.java
+++ app/src/main/java/com/example/projekt1/VotingActivity.java
@@ -1,12 +1,12 @@
 package com.example.projekt1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Adapter;
 import android.widget.Button;
 
 public class VotingActivity extends AppCompatActivity {
@@ -21,7 +21,10 @@ public class VotingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voting);
         recyclerView=findViewById(R.id.vote_recyclerView);
-
+        recyclerView.setLayoutManager(new GridLayoutManager(this,5));
+        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        adapter = new Adapter(this,data);
+        recyclerView.setAdapter(adapter);
         votebutton=findViewById(R.id.vote_button);
         votebutton.setOnClickListener(new View.OnClickListener() {
             @Override
