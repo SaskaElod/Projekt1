5fe7d24e301cad7c29fa57adc120735b7543688b
SaskaElod <saskaelod98@gmail.com>
2019-10-24 15:57:03 +0300

    asdasd

--- app/src/main/java/com/example/projekt1/VotingActivity.java
+++ app/src/main/java/com/example/projekt1/VotingActivity.java
@@ -3,12 +3,22 @@ package com.example.projekt1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VotingActivity extends AppCompatActivity {
 
+    Button votebutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voting);
+        votebutton=findViewById(R.id.vote_button);
+        votebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
