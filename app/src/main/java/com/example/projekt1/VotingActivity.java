c5a67c97d30dd90b516f0cf95ec409b224badfae
albertbrigi123 <albertbrigi95@gmail.com>
2019-10-24 15:58:23 +0300

    Merge branch 'develop' of https://github.com/SaskaElod/Projekt1 into henri
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/projekt1/VotingActivity.java
+++ app/src/main/java/com/example/projekt1/VotingActivity.java
@@ -1,14 +1,23 @@
 package com.example.projekt1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.Button;
 
 public class VotingActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voting);
+        recyclerView=findViewById(R.id.vote_recyclerView);
+        
     }
 }
