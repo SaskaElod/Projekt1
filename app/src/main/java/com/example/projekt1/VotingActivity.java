4cf7202f3584fa0b4147e4076cce70e7ec2a60bd
szocs_000 <szocszoltan814@gmail.com>
2019-10-24 16:06:30 +0300

    proba

--- app/src/main/java/com/example/projekt1/VotingActivity.java
+++ app/src/main/java/com/example/projekt1/VotingActivity.java
@@ -3,12 +3,19 @@ package com.example.projekt1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class VotingActivity extends AppCompatActivity {
+
+
+    Button votebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voting);
+
     }
+
+
 }
