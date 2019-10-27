d68fcbe37bccef6f3e3b0823ac313a43dd560704
SaskaElod <saskaelod98@gmail.com>
2019-10-27 17:25:46 +0200

    Merge branch 'develop' of https://github.com/SaskaElod/Projekt1 into saskaelod
    # Conflicts:
    #	app/src/main/java/com/example/projekt1/VotingActivity.java
    #	app/src/main/res/layout/activity_voting.xml
    #	app/src/main/res/layout/oneitem.xml

--- app/src/main/java/com/example/projekt1/ScoresFragment.java
+++ app/src/main/java/com/example/projekt1/ScoresFragment.java
@@ -3,8 +3,10 @@ package com.example.projekt1;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +17,25 @@ import android.view.ViewGroup;
  */
 public class ScoresFragment extends Fragment {
 
-
+    String score;
     public ScoresFragment() {
         // Required empty public constructor
     }
 
+    public static ScoresFragment newInstance(String text){
+        ScoresFragment scoresFragment=new ScoresFragment();
+        Bundle data = new Bundle();
+        data.putString("Vote",text);
+        scoresFragment.setArguments(data);
+        return scoresFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        score=getArguments().getString("Vote");
+        Log.d("aaaaaaaaaa",score);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
